e75795a11f6566dfe7e05936a808a4b4c012c271
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-19 13:13:17 +0700

    Nhung pug

--- index.ts
+++ index.ts
@@ -4,8 +4,13 @@ const port: Number = 3000;
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Trang chủ");
+//Nhúng pug
+app.set('views', './views');
+app.set('view engine', 'pug');
+//End nhúng pug
+
+app.get("/tours", (req: Request, res: Response) => {
+  res.render('client/tours/index');
 })
 
 app.listen(port, () => {
