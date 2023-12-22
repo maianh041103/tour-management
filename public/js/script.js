24af153a46d16d4704c4eebd157eeb60629ca906
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-22 16:29:16 +0700

    Them tour vao gio hang

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,36 @@ var imagesMain = new Swiper(".imagesMain", {
     swiper: imagesThumb
   },
 });
+
+//Kiem tra co gio hang chua
+const cart = localStorage.getItem("cart");
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+//Thêm tour vào giỏ hàng
+const formAddTourToCart = document.querySelector("[form-add-to-cart]");
+if (formAddTourToCart) {
+  formAddTourToCart.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const tourId = parseInt(formAddTourToCart.getAttribute("tour-id"));
+    const quantity = parseInt(e.target.elements.quantity.value);
+
+    if (quantity > 0 && tourId) {
+      let cart = JSON.parse(localStorage.getItem("cart"));
+      const index = cart.findIndex(item => item.tourId === tourId);
+      if (index !== -1) {
+        cart[index]["quantity"] += quantity;
+      }
+      else {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        });
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  })
+}
+//End thêm tour vào giỏ hàng
