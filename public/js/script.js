60f777ced484725369eefbba172f7ce7539891df
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-22 16:11:25 +0700

    Hien thi anh theo slide

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,16 @@
-console.log("OK");
+var imagesThumb = new Swiper(".imagesThumb", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+});
+
+var imagesMain = new Swiper(".imagesMain", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: imagesThumb
+  },
+});
