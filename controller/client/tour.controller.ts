003f60bb245e5096b6c9bcfb2ebf7420b5f81bda
maianh041103 <nguyenmaianh041103@gmail.com>
2023-12-22 15:25:53 +0700

    Danh sach tour theo danh muc san pham

--- controller/client/tour.controller.ts
+++ controller/client/tour.controller.ts
@@ -1,14 +1,34 @@
 import { Request, Response } from "express";
 import Tour from "../../models/tour.model";
+import sequelize from "../../config/database";
+import { QueryTypes } from "sequelize";
 
 export const index = async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true
-  });
+  const slugCategory = req.params.slugCategory;
+  console.log(slugCategory);
+  const tours = await sequelize.query(`
+  select tours.*, price*(1-discount/100) as price_special
+  from tours
+  join tours_categories on tours.id = tours_categories.tour_id
+  join categories on categories.id = tours_categories.category_id
+  where 
+    tours.deleted = false 
+    and tours.status = 'active'
+    and categories.slug = '${slugCategory}'
+    and categories.status = 'active'
+  `, { type: QueryTypes.SELECT })
+
+  tours.forEach(item => {
+    if (item["images"]) {
+      item["images"] = JSON.parse(item["images"]);
+      item["image"] = item["images"][0];
+    }
+    else
+      item["image"] = "";
+    item["price_special"] = parseInt(item["price_special"]);
+  })
+
+  console.log(tours);
 
   res.render('client/pages/tours/index', {
     title: "Danh sách tour",
--- route/client/tour.route.ts
+++ route/client/tour.route.ts
@@ -3,6 +3,6 @@ import * as controller from "../../controller/client/tour.controller";
 
 const route: Router = express.Router();
 
-route.get("/", controller.index);
+route.get("/:slugCategory", controller.index);
 
 export const TourRoutes: Router = route;
