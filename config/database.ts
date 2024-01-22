9b9f1ad95306be5605fb21aeee58b3b65f89f057
maianh041103 <nguyenmaianh041103@gmail.com>
2024-01-22 23:26:50 +0700

    Sửa connect db

--- config/database.ts
+++ config/database.ts
@@ -7,7 +7,7 @@ const sequelize: Sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
     dialectModule: require('mysql2')
--- dist/config/database.js
+++ dist/config/database.js
@@ -7,7 +7,7 @@ const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
     dialectModule: require('mysql2')
